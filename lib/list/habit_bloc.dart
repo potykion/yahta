6c978655e8a45c4243125afa0b6041a5453c47d4
potykion <potykion@gmail.com>
2020-04-25 10:20:45 +0300

    Restructuring

--- lib/list/habit_bloc.dart
+++ lib/list/habit_bloc.dart
@@ -8,64 +8,13 @@ import 'package:collection/collection.dart';
 // MODELS AND MAPPINGS
 ////////////////////////////////////////////////////////////////////////////////
 
-
 enum CompletionStatus { positive, neutral, negative }
-
-computeDateRelationCompletionStatus(
-    List<Habit> habits,
-    List<HabitMark> habitMarks,
-    DateRelation dateRelation,
-    ) {
-  var allHabitIds = uniqueHabitIds(habits: habits);
-  var completedHabitIds = uniqueHabitIds(
-    habitMarks: filterDateRelationHabitMarks(habitMarks, dateRelation),
-  );
-
-  if (SetEquality().equals(allHabitIds, completedHabitIds)) {
-    return CompletionStatus.positive;
-  }
-  else if (completedHabitIds.length == 0) {
-    return CompletionStatus.negative;
-  }
-  else {
-    return CompletionStatus.neutral;
-  }
-}
-
-Set<int> uniqueHabitIds({List<Habit> habits, List<HabitMark> habitMarks}) =>
-    ((habits ?? []).map((h) => h.id).toList() +
-        (habitMarks ?? []).map((h) => h.habitId).toList())
-        .toSet();
-
-List<HabitMark> filterDateRelationHabitMarks(
-    List<HabitMark> habitMarks, DateRelation dateRelation) =>
-    habitMarks
-        .where((hm) =>
-        dateRelationToDateRange(dateRelation).matchDatetime(hm.datetime))
-        .toList();
-
-DayDateTimeRange dateRelationToDateRange(DateRelation dateRelation) =>
-    DayDateTimeRange(DateTime.now().add(dateRelationToDuration(dateRelation)));
-
-Duration dateRelationToDuration(DateRelation dateRelation) {
-  switch (dateRelation) {
-    case DateRelation.today:
-      return Duration(days: 0);
-    case DateRelation.yesterday:
-      return Duration(days: -1);
-    case DateRelation.twoDaysAgo:
-      return Duration(days: -2);
-    default:
-      throw ArgumentError("wtf: $dateRelation");
-  }
-}
 
 Map<CompletionStatus, Color> StatusToColorMap = {
   CompletionStatus.positive: Color(0xff95E1D3),
   CompletionStatus.neutral: Color(0xffFCE38A),
   CompletionStatus.negative: Color(0xffF88181),
 };
-
 
 enum DateRelation { today, yesterday, twoDaysAgo }
 
@@ -141,6 +90,41 @@ class HabitState {
           ),
         ),
       );
+
+  computeDateRelationCompletionStatus(
+    List<Habit> habits,
+    List<HabitMark> habitMarks,
+    DateRelation dateRelation,
+  ) {
+    var allHabitIds = uniqueHabitIds(habits: habits);
+    var completedHabitIds = uniqueHabitIds(
+      habitMarks: filterDateRelationHabitMarks(habitMarks, dateRelation),
+    );
+
+    if (SetEquality().equals(allHabitIds, completedHabitIds)) {
+      return CompletionStatus.positive;
+    } else if (completedHabitIds.length == 0) {
+      return CompletionStatus.negative;
+    } else {
+      return CompletionStatus.neutral;
+    }
+  }
+
+  Set<int> uniqueHabitIds({List<Habit> habits, List<HabitMark> habitMarks}) =>
+      ((habits ?? []).map((h) => h.id).toList() +
+              (habitMarks ?? []).map((h) => h.habitId).toList())
+          .toSet();
+
+  List<HabitMark> filterDateRelationHabitMarks(
+          List<HabitMark> habitMarks, DateRelation dateRelation) =>
+      habitMarks
+          .where((hm) =>
+              dateRelationToDateRange(dateRelation).matchDatetime(hm.datetime))
+          .toList();
+
+  DayDateTimeRange dateRelationToDateRange(DateRelation dateRelation) =>
+      DayDateTimeRange(
+          DateTime.now().add(DateRelationToDuration[dateRelation]));
 
   DayDateTimeRange get dateRelationDateRange => DayDateTimeRange(
         DateTime.now().add(DateRelationToDuration[selectedDateRelation]),
